5019d1bc2a7b5a16868764e13e4f83456bb6b7a5
shrutirthr29 <shray.shruti@gmail.com>
2024-12-04 12:43:43 +0530

    project completed

---
+++ components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div className='text-2xl font-semibold'>
+        <span className='bg-[#F4A424]  text-black rounded-md px-2'>D</span> <span className='text-[#F4A424]'>RIDE</span>
+    </div>
+  )
+}
+
+export default Logo
